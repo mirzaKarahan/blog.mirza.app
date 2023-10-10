57a8fe695f8ff208af01e2d6b15860abcd6f236e
Mirza KARAHAN <mirzaakarahan@gmail.com>
2023-10-10 23:33:35 +0300

    post eklendi

--- postGenerator.py
+++ postGenerator.py
@@ -14,7 +14,7 @@ def generatedPostContentFromChatGTP(subject):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
          messages=[
-             {"role": "user", "content": "Şimdi bana "+subject+" Bu blog makalesini yazarken bana sadece makalenin içeriğini mdx formatında ve olabildiğince detaylı bir şekilde vermeni istiyorum. Makalenin en başında alt alta [---\n title: {Makalenin başlığı}\nkeywords: {SEO için anahtar kelimeler}\ndate:{yil-ay-gun formatında tarih}\ndescription: {açıklaması}.\n ---] yazmalı. Tüm makale baştan sona ingilizce dilinde olmalı."}
+             {"role": "user", "content": "Şimdi bana "+subject+" Bu blog makalesini yazarken bana sadece makalenin içeriğini mdx formatında ve olabildiğince detaylı bir şekilde vermeni istiyorum. Makalenin en başında alt alta [---\n title: {Makalenin başlığı}\nkeywords: {SEO için anahtar kelimeler}\ndate:{yil-ay-gun formatında tarih}\ndescription: {açıklaması}.\n ---] yazmalı."}
              ]
     )
     return response.choices[0].message['content']
@@ -32,23 +32,7 @@ def changedFilesGitPushCommandRun(message):
 
 #postContent = generatedPostContentFromChatGTP(".Net Core ile bir REST API web servisinin nin nasıl yazılacağını anlatan bir blog yazmanı istiyorum.");
 
-yazilim_sorunlari = [ "Software Development Processes: Agile vs. Waterfall Comparison",
-  "Web Development: Front-end vs. Back-end Differences",
-  "Database Design and Normalization Fundamentals",
-  "Introduction to Python Programming: Basic Concepts and Language Features",
-  "Mobile App Development: Android vs. iOS",
-  "Software Testing Automation: Best Practices and Tools",
-  "Software Security: Common Threats and Protection Strategies",
-  "Artificial Intelligence and Machine Learning: Fundamental Principles and Applications",
-  "Software Architecture: Monolithic vs. Microservices Architecture Comparison",
-  "Blockchain Technology and the Future of Cryptocurrencies",
-  "The Importance of Open Source Software and a Guide to Contributing",
-  "Best Practices for Software Development and Code Quality",
-  "Digital Product Design: User Experience and User Interface Design",
-  "Data Analytics and Big Data: Data Mining and Data Visualization",
-  "Internet of Things (IoT) and Smart Devices: The Technology of the Future",
-  "Tips for a Software Career: Paths to Becoming a Successful Software Developer",
-]
+yazilim_sorunlari = [".Net Core ile bir REST API web servisinin nin nasıl yazılır","İsrail ile Filistin arasındaki gerilim"]
 
 for item in yazilim_sorunlari:
     postContent = generatedPostContentFromChatGTP(item+" konusu hakkında blog yazmanı istiyorum.");
